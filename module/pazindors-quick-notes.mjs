c8bff3f48368bbf0f3d4318b0cc1b1ba217285be
pazindorb <xxpazindorxx@gmail.com>
2025-07-16 15:05:21 +0200

    Innitial Commit

---
+++ module/pazindors-quick-notes.mjs
@@ -0,0 +1,15 @@
+Hooks.on("getSceneControlButtons", (controls) => {
+  console.log("=================================== PAZINDOR'S QUICK NOTES ===================================")
+
+  addQuickNotesButton();
+  console.log(controls)
+
+
+})
+
+function addQuickNotesButton() {
+
+  controls.pazindorsQuickNotes = {
+    
+  }
+}
